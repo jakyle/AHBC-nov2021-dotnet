624699c7ce189bab7a6fb9fdef23225c1c770b6d
Jakyle <jjackson360@gmail.com>
2021-12-08 21:41:32 -0500

    more functions

--- Functions/Functions/Program.cs
+++ Functions/Functions/Program.cs
@@ -1,6 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
+//    vvCalling function|vv arguments
+int sum = SumTwoNumbers(2, 3); // order of the arguments passed in the function call, MATTERS, it maps 1:1 with the order of the parameters
+int sumTwo = SumTwoNumbers(3, 4);
+Console.WriteLine(sum);
+
 // Functions
 //vvOutput|vvName|  vvInput (parameters)
 int SumTwoNumbers(int x, int y) // Parameters are placeholders for the ACTUAL input that will be sent in
@@ -10,13 +15,9 @@ int SumTwoNumbers(int x, int y) // Parameters are placeholders for the ACTUAL in
     return z; // The RETURN keyword KICKS us out of the function call AND "RETURNS" the OUTPUT WITH IT
 }
 
-//    vvCalling function|vv arguments
-int sum = SumTwoNumbers(2, 3); // order of the arguments passed in the function call, MATTERS, it maps 1:1 with the order of the parameters
-int sumTwo = SumTwoNumbers(3, 4);
-Console.WriteLine(sum);
-
-
-
+/// <summary>
+/// /////////////////////////////////
+/// </summary>
 string GetUserInput(string userMessage) // parameters are optional
 {
     Console.WriteLine(userMessage);
@@ -24,27 +25,87 @@ string GetUserInput(string userMessage) // parameters are optional
     return userInput;
 }
 
-bool CanLegallyDrink(int age)
+bool CanLegallyDrink(int userAge)
 {
-    return age >= 21;
+    // passing in an age that will check for an inaquality
+    // that will determine if the user can drink or not
+    if (userAge > 20)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
 }
 
+
+// a string is anything wrapped in double quotes, ie. "sup guys" <--- this is a string
 string GetDrinkingMessage(bool canDrink)
 {
     if (canDrink)
     {
-        return "You can drink, what will you have?";
+        return "You can drink if you want to, you can bring your friends along!";
     }
-    else
-    {
-        return "Get the fuck outta here!";
-    }
-}
 
+    return "um, you can't drink... get outta here!";
+}  // <--- defining the INPUT for our function 
+
+// the actual program ""
 string userInput = GetUserInput("Give me your age!"); // When calling a function, simply Write the name of the function
 // followed by paranthesis and any "arguments" the function takes in.
 
+
+// step 1   
+// step 2
+// step 3
+//
+//
+//
 int userAge = int.Parse(userInput);
 bool canDrink = CanLegallyDrink(userAge);
 string drinkingMessage = GetDrinkingMessage(canDrink);
 Console.WriteLine(drinkingMessage);
+////
+///
+// Validation Functions
+
+
+int GetValidPositiveNumber(string messageForUser)
+{
+    while (true)
+    {
+        string userInputTwo = GetUserInput(messageForUser);
+
+        if (int.TryParse(userInputTwo, out int age) && age > 0)
+        {
+            return age;
+        }
+
+        Console.WriteLine("Invalid age");
+    }
+}
+
+// Recursion... is it confusing? fair... moving on
+int CallMySelfAgain(int number)
+{
+    if (number <= 0)
+    {
+        return 0;
+    }
+
+    return CallMySelfAgain(number - 1);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
