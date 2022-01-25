c4c8680ae24100145ebcd474c187c79bb25b8500
Jakyle <jjackson360@gmail.com>
2022-01-24 21:48:39 -0500

    stacks and queues

---
+++ looping-review/looping-review/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace looping_review
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var names = new List<string>();
+            names.Add("James");
+            names.Add("Coco");
+            names.Add("Browny");
+
+            var count = CountOfEvenNames(names);
+        }
+
+        public static int CountOfEvenNames(IEnumerable<string> names)
+        {
+            int count = 0;
+            try
+            {
+                foreach (string name in names)
+                {
+                    if (name.Length % 2 == 0)
+                    {
+                        count++;
+                    }
+                }
+                return count;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            return count;            
+        }
+
+    }
+}
---
+++ stacksandqueues/stacksandqueues/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace stacksandqueues
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Queue, FIFO -> First in First out
+            var nameQueue = new Queue<string>();
+
+
+            // entering your "line" or queue
+            nameQueue.Enqueue("James");
+            nameQueue.Enqueue("Kyle");
+            nameQueue.Enqueue("Corn");
+            nameQueue.Enqueue("Bob");
+            nameQueue.Enqueue("Carson");
+            nameQueue.Enqueue("Leon");
+
+
+            Console.WriteLine(nameQueue.Peek());
+
+            var name = nameQueue.Dequeue();
+
+            Console.WriteLine(name);
+
+           // Stack, LIFO
+           var nameStack = new Stack<string>();
+
+
+            // adding to the stack
+            nameStack.Push("James");
+            nameStack.Push("Kyle");
+            nameStack.Push("Corn");
+            nameStack.Push("Bob");
+            nameStack.Push("Carson");
+            nameStack.Push("Leon");
+
+            // remove or "pop" from the stack
+            nameStack.Pop();
+            nameStack.Pop();
+            var currentName = nameStack.Pop();
+
+            Console.WriteLine(currentName);
+
+            Console.ReadLine();
+        }
+    }
+}
